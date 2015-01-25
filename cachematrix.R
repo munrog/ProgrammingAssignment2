333d4046f706da1e18e1bbe98596f5fa89b5a555
munrog <munrog@users.noreply.github.com>
2015-01-25 22:45:31 +0800

    ran some business on matrices using inverses with the global power team to test the new version of the archaic build

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## Functions: makeCacheMatrix
+##            cacheSolve
 
-## Write a short comment describing this function
+
+## makeCacheMatrix
+##  input:  a square matrix
+##  output: list including fuctions for acesses to the matrix and it's 
+##          inverse
+##  Use cacheSolve() to solve the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## cacheSolve
+##  input list created by makeCacheMatrix()
+##  output  returns the inverse of maxtrix supplied in the input. 
+##          
+##  The inverse is only calculated once otherwise a cached version is returned.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {       
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
